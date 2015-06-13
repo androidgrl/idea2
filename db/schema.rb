c4c554d801cffd7c04a831f021ff7626f370df01
androidgrl <kawaharajamie@gmail.com>
2015-06-13 14:48:24 -0600

    Add Idea model
    *Add Idea model
    *Add model tests

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,17 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613203035) do
+ActiveRecord::Schema.define(version: 20150613204340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -10,6 +22,18 @@ ActiveRecord::Schema.define(version: 20150613203035) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ideas", force: :cascade do |t|
+    t.text     "title"
+    t.text     "description"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "ideas", ["category_id"], name: "index_ideas_on_category_id", using: :btree
+  add_index "ideas", ["user_id"], name: "index_ideas_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.text     "username"
     t.text     "password_digest"
@@ -18,4 +42,6 @@ ActiveRecord::Schema.define(version: 20150613203035) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "ideas", "categories"
+  add_foreign_key "ideas", "users"
 end
