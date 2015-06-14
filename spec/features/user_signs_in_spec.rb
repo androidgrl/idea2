16c9f79b92cd2d9969f88c3489e877db5f7b57e5
androidgrl <kawaharajamie@gmail.com>
2015-06-13 22:26:12 -0600

    Move expect_user_to_be_signed_in method
    I want the above method in the rails_helper file
    so that I can use it in many tests.

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -2,28 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "User signs in" do
   scenario "successfully" do
-    visit root_path
-    click_on "Sign Up"
+    visit sign_in_path
 
     fill_in "Username", with: "Jamie"
     fill_in "Password", with: "password"
     within "form" do
-      click_on "Sign Up"
+      click_on "Sign In"
     end
 
     expect_user_to_be_signed_in(as: "Jamie")
-    # Then I see my User page with a list of my ideas, and a link to add a new idea.
-    # And I click on "Add idea",
-    # And I fill in the form's title and description fields and I select the category that the idea belongs to,  and I select the image that the idea belongs to,
-    # Then I see my User page again with the new idea added and the image and
   end
-
-  private
-
-  def expect_user_to_be_signed_in(as:)
-    expect(page).not_to have_content("Sign In")
-    expect(page).not_to have_content("Sign Up")
-    expect(page).to have_content("Sign Out")
-    expect(page).to have_content(as)
-  end
+  # Then I see my User page with a list of my ideas, and a link to add a new idea.
+  # And I click on "Add idea",
+  # And I fill in the form's title and description fields and I select the category that the idea belongs to,  and I select the image that the idea belongs to,
+  # Then I see my User page again with the new idea added and the image and
 end
