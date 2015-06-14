e6955f3b5dc7c51916dcba9af3dd92f4a914b368
androidgrl <kawaharajamie@gmail.com>
2015-06-14 01:01:55 -0600

    Add a new idea test

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -30,7 +30,22 @@ RSpec.feature "User signs in" do
     expect(page).to have_content("Free Art")
     expect(page).to have_link("Add Idea", href: new_idea_path)
   end
-  # And I click on "Add idea",
+
+  xit 'a user can add an idea' do
+    visit sign_in_path
+
+    fill_in "Username", with: "Jamie"
+    fill_in "Password", with: "password"
+    within "form" do
+      click_on "Sign In"
+    end
+
+    click_on "Add Idea"
+    fill_in "Title", with: "Free Art"
+    fill_in "Description", with: "Make art free day"
+
+  end
+
   # And I fill in the form's title and description fields and I select the category that the idea belongs to,  and I select the image that the idea belongs to,
   # Then I see my User page again with the new idea added and the image and
 end
