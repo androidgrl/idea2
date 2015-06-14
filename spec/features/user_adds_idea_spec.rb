8ec1bb428415e0aed5bbbcf120289c2caaf88aa7
androidgrl <kawaharajamie@gmail.com>
2015-06-14 16:34:55 -0600

    Remove commented out mocking

--- spec/features/user_adds_idea_spec.rb
+++ spec/features/user_adds_idea_spec.rb
@@ -8,50 +8,37 @@ RSpec.feature "User adds idea" do
   end
 
   it 'a user can add an idea' do
-    #user = User.create(username: "Jamie",
-    #password: "password",
-    #role: 0)
-
-    #ApplicationController.any_instance.stub(:current_user).and_return(user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     visit user_path(@user)
-
-    #visit sign_in_path
-    #fill_in "Username", with: "Jamie"
-    #fill_in "Password", with: "password"
-    #within "form" do
-      #click_on "Sign In"
-    #end
 
     click_on "Add Idea"
     fill_in "Title", with: "Free Art"
     fill_in "Description", with: "Make art free day"
     click_on "Submit Idea"
-
     @user.reload
-
     visit user_path(@user)
+
     expect(page).to have_content("Jamie's Ideas:")
     expect(page).to have_content("Free Art")
   end
 
   it 'user can edit an idea' do
-    visit sign_in_path
-    fill_in "Username", with: "Jamie"
-    fill_in "Password", with: "password"
-    within "form" do
-      click_on "Sign In"
-    end
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    visit user_path(@user)
 
     click_on "Add Idea"
     fill_in "Title", with: "Free Art"
     fill_in "Description", with: "Make art free day"
     click_on "Submit Idea"
+    @user.reload
+    visit user_path(@user)
 
     click_on "Edit"
     fill_in "Title", with: "Free Art in the Park"
     fill_in "Description", with: "Have free art demos in the park"
     click_on "Submit Idea"
+    @user.reload
+    visit user_path(@user)
 
     expect(page).to have_content("Jamie's Ideas:")
     expect(page).to have_content("Have free art demos in the park")
